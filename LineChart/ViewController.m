23c210cbf984243234ac8ead405c7eebc9b4a8c4
zhangzhihua19920219 <406608969@qq.com>
2016-07-21 10:49:24 +0800

    update

--- LineChart/ViewController.m
+++ LineChart/ViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "ViewController.h"
-
+#import "LineView.h"
+#import "LineDataModel.h"
+#define MAXVALUE  @"0"
+#define MINVALUE @"999"
 @interface ViewController ()
+
+@property (nonatomic , strong)LineView *lineView;
+
 
 @end
 
@@ -17,6 +23,22 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self create_View];
+}
+
+-(void)create_View{
+    _lineView = [[LineView alloc]initWithFrame:CGRectMake(0, 20, self.view.frame.size.width, self.view.frame.size.height/2  + 40)];
+    [self.view addSubview:_lineView];
+    
+    NSMutableArray *modelArray = [[NSMutableArray alloc]init];
+    
+    for(int i = 0 ; i<5 ; i++){
+        LineDataModel *model = [[LineDataModel alloc]init];
+        model.xValue = [NSString stringWithFormat:@"%u",arc4random()%20];
+        model.yValue = [NSString stringWithFormat:@"%u",arc4random()%20];
+        [modelArray addObject:model];
+    }
+    [_lineView setPointValue:modelArray andMaxValue:@"20" andMinValue:@"0" andTitleString:@"IF指数"];
 }
 
 - (void)didReceiveMemoryWarning {
